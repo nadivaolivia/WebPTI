3eb3496dd4f28604818a4d6c07de69cdb449c865
unknown <nadivaolivia337@gmail.com>
2024-07-19 16:25:02 +0700

    Pesan commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import Navbar from "./components/Navbar";
+import home from "./components/home";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
---
+++ src/Welcome.jsx
@@ -0,0 +1,10 @@
+const Welcome = (props) => {
+    return (
+        <div classname="ml-20 text-center mt-10">
+
+            <h1 className="text-5xl font-bold">Selamat Datang di Web PTI</h1>
+        </div>
+    );
+};
+
+export default Welcome;
