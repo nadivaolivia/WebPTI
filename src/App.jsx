8b65a29b75acc4c28d48feaf400f5e56e88d19b8
unknown <nadivaolivia337@gmail.com>
2024-07-19 16:36:37 +0700

    Deskripsi perubahan

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import Navbar from "./components/Navbar";
-import home from "./components/home";
+import Home from "./components/home";
 import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div className="overflow-x-hidden text-neutral-300">
       <div className="fixed -z-10 top-0 h-full w-full">
-        <div class="absolute top-0 z-[-2] h-screen w-screen bg-gradient-to-r from-teal-400 to-gray-800"></div>
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-gradient-to-r from-teal-400 to-gray-800"></div>
       </div>
 
       <div className="mx-auto">
         <Navbar />
+        <Home />
+        <Welcome />
       </div>
     </div>
   );
